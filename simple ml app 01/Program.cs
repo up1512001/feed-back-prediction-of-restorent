ed292706bf6028f23431315ca90b5036147dd789
up1512001 <utsav.patel1215@outlook.com>
2021-08-04 23:36:33 +0530

    First Commit

---
+++ simple ml app 01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Simple_ml_app_01ML.Model;
+
+namespace simple_ml_app_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new ModelInput()
+            {
+                Col0 = "This is  good restaurant."
+            };
+            ModelOutput result = ConsumeModel.Predict(input);
+            string sentiment = result.Prediction == "1" ? "Positive" : "Negative";
+            Console.WriteLine($"Text : {input.Col0}\nSentiment : {sentiment}");
+        }
+    }
+}
